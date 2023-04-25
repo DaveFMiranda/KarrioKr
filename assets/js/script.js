fdf9b6109698549b65c1a5a99573eda6cf45c6c9
Tommy Ho <tommyho@Tommys-MBP.attlocal.net>
2023-04-25 11:55:26 -0500

    updated html

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// input variable that receives text string from HTML input box
+// create element for search button
+
+var input = document.querySelector(".input");
+
+
+// YT data API key: AIzaSyBHj9EPuptQC5RJjuRdyrT3jmI82emkqlo
+
+
+// send input.text value to YT data API
+
+
+/* event listener for search button...
+        1) triggers YT data API
+        2) returns list of links
+        3) triggers lyrics API
+        4) stores lyrics API
+        5) triggers dynamic creation of JS search results box
+        */
+
+/* talk to YT data site via API
+1) send search string
+2) GET results
+3) display results in new box
+*/
+
+
+// CODE THAT TALKS TO YT VIDEO PLAYER
+
+
+// CODE THAT CREATES CONTAINER TO PLAY THE VIDEO IN
+
+
+// CODE THAT RETRIEVES LYRICS FROM MUSIXMATCH
+
+// CODE THAT CREATES LYRICS BOX
